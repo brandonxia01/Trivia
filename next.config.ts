181b94dd65f429a552ec18d807f2dca20f335e25
Brandon Xia <brandonxia01@gmail.com>
2025-10-31 12:11:34 -0400

    testt

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: 'export', // Enables static HTML export
 };
 
 export default nextConfig;
